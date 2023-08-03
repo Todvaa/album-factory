419f565eafa7e882c4075b86ca48fb3e92a53f9d
Varlamov Anton <aa.varlamov1997@gmail.com>
2023-08-03 12:50:17 +0530

    AF-27 PhotosProcessor. Face recognizer

--- photos_processor/main.py
+++ photos_processor/main.py
@@ -1,5 +1,23 @@
-from photos_processor.classificators import Classificator
-from photos_processor.recognizers import Recognizer
+import os
+
+from dotenv import load_dotenv
+from propan import RabbitBroker, PropanApp
+from propan.brokers.rabbit import RabbitExchange, RabbitQueue, ExchangeType
+
+from classificators import Classificator
+from recognizers import Recognizer
+
+load_dotenv()
+
+RABBITMQ_DEFAULT_USER = os.getenv('RABBITMQ_DEFAULT_USER')
+RABBITMQ_DEFAULT_PASS = os.getenv('RABBITMQ_DEFAULT_PASS')
+RABBITMQ_PORT = os.getenv('RABBITMQ_PORT')
+broker = RabbitBroker(f'amqp://{RABBITMQ_DEFAULT_USER}:{RABBITMQ_DEFAULT_PASS}@localhost:{RABBITMQ_PORT}/')
+app = PropanApp(broker)
+exchange = RabbitExchange('album_factory_exchange', type=ExchangeType.DIRECT)
+
+photo_processing_queue = RabbitQueue('photos_processing')
+photos_processed_queue = RabbitQueue('photos_processed')
 
 
 def run():
@@ -9,8 +27,8 @@ def run():
     persons_vectors = recognizer.run()
     classificator = Classificator(persons_vectors)
     persons = classificator.run()
-    # todo: сделать слушателя = консьюмер photos_procesfasfaing
-    # todo: положить в photos_prsdgsdgsed [persons_vectors, persons]
+    # todo: сделать слушателя = консьюмер photo_processing
+    # todo: положить в photos_processed [persons_vectors, persons]
 
 if __name__ == "__main__":
     run()
