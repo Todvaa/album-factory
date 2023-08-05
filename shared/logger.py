7d85990eaa6de44b4599ed34e9cfb01f7fdc5686
Varlamov Anton <aa.varlamov1997@gmail.com>
2023-08-05 20:47:30 +0530

    AF-25 Photos Downloader

--- shared/logger.py
+++ shared/logger.py
@@ -3,9 +3,12 @@ import logging
 
 class Logger:
     def __init__(self, level=logging.INFO):
-        self.logger = logging.getLogger('my_logger')
+        self.logger = logging.getLogger('stdout_logger')
         self.logger.setLevel(level)
-        formatter = logging.Formatter('[%(levelname)s] %(asctime)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+        formatter = logging.Formatter(
+            '[%(levelname)s] %(asctime)s - %(message)s',
+            datefmt='%Y-%m-%d %H:%M:%S'
+        )
         console_handler = logging.StreamHandler()
         console_handler.setFormatter(formatter)
         self.logger.addHandler(console_handler)
