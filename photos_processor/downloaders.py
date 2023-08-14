40a2dea6195fa46cf9786110fd7a3361a7703cf9
Varlamov Anton <aa.varlamov1997@gmail.com>
2023-08-15 02:41:07 +0800

    AF-27 PhotosProcessor. Face recognizer

--- photos_processor/downloaders.py
+++ photos_processor/downloaders.py
@@ -3,17 +3,9 @@ import shutil
 import tempfile
 from abc import ABC, abstractmethod
 
-from dotenv import load_dotenv
-from s3fs import S3FileSystem
-
 from constants import MODULE_NAME
 from shared.logger import logger
-
-load_dotenv()
-
-S3_ROOT_USER = os.getenv('S3_ROOT_USER')
-S3_ROOT_PASSWORD = os.getenv('S3_ROOT_PASSWORD')
-S3_URL = os.getenv('S3_URL')
+from shared.s3 import FILE_SYSTEM
 
 
 class AbstractDownloader(ABC):
@@ -32,14 +24,11 @@ class AbstractDownloader(ABC):
 
 
 class S3Downloader(AbstractDownloader):
-    FILE_SYSTEM = S3FileSystem(
-        key=S3_ROOT_USER, secret=S3_ROOT_PASSWORD,
-        endpoint_url=S3_URL
-    )
+    file_system = FILE_SYSTEM
 
     def run(self) -> str:
         logger.info(module=MODULE_NAME, message='start downloading from S3')
-        self.FILE_SYSTEM.get(
+        self.file_system.get(
             self.s3_path,
             self.downloads_dir, recursive=True
         )
