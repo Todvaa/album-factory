dd0c266d24297a29120a3370d05f44397e569a8a
vsokolov <vsokolov@rbc.ru>
2023-08-16 23:39:24 +0300

    eyes detection

--- tests/photos_processor/recognizer.py
+++ tests/photos_processor/recognizer.py
@@ -54,6 +54,16 @@ class RecognizerTest(unittest.TestCase):
             self.assertEqual(1, recognized_photo.face_count)
             self.assertEqual(1, len(recognized_photo.vectors))
 
+    # def test_group_face(self):
+    #     photo_names = ['group.png']
+    #     photos = [Photo(photo_name, '') for photo_name in photo_names]
+    #
+    #     recognizer = recognizers.Recognizer('photos', photos)
+    #     recognized_photos = recognizer.run()
+    #     self.assertEqual(1, len(recognized_photos))
+    #     self.assertEqual(5, recognized_photos[0].face_count)
+    #     self.assertEqual(5, len(recognized_photos[0].vectors))
+
     def test_non_face(self):
         recognizer = recognizers.Recognizer('photos', [Photo('non_face.jpeg', '')])
         photos = recognizer.run()
